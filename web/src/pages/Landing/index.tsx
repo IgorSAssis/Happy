5b2179ec17f582dfec85c248bc599a861a2f0e9b
IgorSA <igorsemphoski@gmail.com>
2020-10-12 16:43:26 -0300

    :package: Imp: Creating the landing page

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+import "./styles.css"
+import logoImg from "../../assets/Logo.svg";
+
+import { FiArrowRight } from "react-icons/fi"
+
+function Landing() {
+    return (
+        <div id="page-landing">
+            <div className="content-wrapper">
+                <img src={logoImg} alt="Happy" ></img>
+
+                <main>
+                    <h1>Leve felicidade para o mundo</h1>
+                    <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+                </main>
+
+                <div className="location">
+                    <strong>Blumenau</strong>
+                    <span>Santa Catarina</span>
+                </div>
+
+                <Link to="/orphanages" className="enter-app">
+                    <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+                </Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
